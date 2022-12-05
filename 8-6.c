711e7e677c9cba53ed98db1647a97a423cf7375b
DexterLab5 <97889250+DexterLab5@users.noreply.github.com>
2022-12-05 11:26:16 +0200

    Add files via upload

---
+++ 8-6.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *calloc2(size_t items, size_t size) {
+    if (items == 0 || size == 0)
+        return NULL;
+    
+    void *mem = malloc(items * size);
+    memset(mem, 0x00, items * size);
+    return mem;
+}
+
+void printarr(int*);
+
+int main(void) {
+    int *arr = calloc2(5, sizeof(int));
+    printarr(arr);
+    
+    for (int i = 0; i < 5; i++)
+        arr[i] = i + 1;
+    
+    printarr(arr);
+}
+
+void printarr(int *arr) {
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
